fe0b8bc419d5b0099499422b55a562697d5e30cb
ashfak0046 <ashfakmahbub006@gmail.com>
2021-05-09 16:51:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn:false,
+    name:'',
+    email:'',
+    photo:''
+  })
+  var googleProvider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () =>{
+    // firebase.auth().signInWithPopup(provider)
+    // .then(res => {
+    //   const {displayName,photoURL,email} = res.user;
+    //     const singedInUser = {
+    //       isSignedIn:true,
+    //       name:displayName,
+    //       email:email,
+    //       photo:photoURL
+    //     }
+    //     setUser(singedInUser);
+    //     console.log(displayName);
+    // })
+    // .catch(error => {
+    //   console.log(error)
+    //   console.log(error.message)
+    //   console.log(error.credential)
+    // })
+    firebase.auth().signInWithPopup(googleProvider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage)
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={handleSignIn}>Sign in</button>
+        {
+          user.isSignedIn && <p>Welcome, {user.name} </p>
+        }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSPTLLKc_9nIaScmCuDGfwKd_Et9gOTQg",
+  authDomain: "ema-john-simple-dff71.firebaseapp.com",
+  projectId: "ema-john-simple-dff71",
+  storageBucket: "ema-john-simple-dff71.appspot.com",
+  messagingSenderId: "261695553528",
+  appId: "1:261695553528:web:c6558914a114f9dccbe00d"
+};
+  export default firebaseConfig;
